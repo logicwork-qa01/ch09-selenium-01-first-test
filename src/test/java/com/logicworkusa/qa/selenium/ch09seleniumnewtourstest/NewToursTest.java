c753f1fabcf09f9eb6018e9b6e7e58eb94a5234c
Saifur Rahman <saifur.rahman@objectstream.com>
2024-01-22 00:31:05 -0600

    Improve code

--- src/test/java/com/logicworkusa/qa/selenium/ch09seleniumnewtourstest/NewToursTest.java
+++ src/test/java/com/logicworkusa/qa/selenium/ch09seleniumnewtourstest/NewToursTest.java
@@ -1,13 +1,13 @@
 package com.logicworkusa.qa.selenium.ch09seleniumnewtourstest;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 public class NewToursTest {
 
